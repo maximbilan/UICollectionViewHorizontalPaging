6257d6069254e5ea6aa9638fee36c3e203635047
Maxim Bilan <maximb.mail@gmail.com>
2016-02-05 10:32:39 +0200

    Added project template

---
+++ UICollectionViewHorizontalPaging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UICollectionViewHorizontalPaging
+//
+//  Created by Maxim Bilan on 2/5/16.
+//  Copyright © 2016 Maxim Bilan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
