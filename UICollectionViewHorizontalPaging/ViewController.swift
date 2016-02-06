9dcd432fa8fc28a5e77fecb118a73d6fab185e9e
Maxim Bilan <maximb.mail@gmail.com>
2016-02-06 18:22:38 +0200

    Added simple collection view

--- UICollectionViewHorizontalPaging/ViewController.swift
+++ UICollectionViewHorizontalPaging/ViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CollectionViewCell : UICollectionViewCell {
+	@IBOutlet weak var imageView: UIImageView!
+}
 
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+	let reuseIdentifier = "collectionViewCellId"
+	
+	@IBOutlet weak var collectionView: UICollectionView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,23 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	// MARK: - UICollectionViewDataSource
+	
+	func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+		return 1
+	}
+	
+	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 6
+	}
+	
+	func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! CollectionViewCell
+		
+		cell.imageView.backgroundColor = UIColor.redColor()
+		
+		return cell
+	}
 
 }
 
